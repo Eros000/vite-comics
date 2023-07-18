c2b223c57e629cf60c03c32fb396a39aa5d5ec1a
Eros000 <e.ghilardi@hotmail.it>
2023-07-18 08:42:20 +0200

    footer in progress

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -86,28 +86,146 @@ export default {
     <div class="footer-center">
 
       <div class="footer-center-nav">
-        <div class="links1">
 
-        </div>
-
-        <div class="links1">
+        <div class="links">
           <h3>
             DC COMICS
           </h3>
 
           <ul>
+
             <li>
-              <a href=""></a>
+              <a href="">Characters</a>
             </li>
+
+            <li>
+              <a href="">Comics</a>
+            </li>
+
+            <li>
+              <a href="">Movies</a>
+            </li>
+
+            <li>
+              <a href="">TV</a>
+            </li>
+
+            <li>
+              <a href="">Games</a>
+            </li>
+
+            <li>
+              <a href="">Videos</a>
+            </li>
+
+            <li>
+              <a href="">News</a>
+            </li>
+
+
+          </ul>
+
+          <h3>
+            SHOP
+          </h3>
+
+          <ul>
+
+            <li>
+              <a href="">Shop DC</a>
+            </li>
+
+            <li>
+              <a href="">Shop DC Collectibles</a>
+            </li>
+
+          </ul>
+
+        </div>
+
+        <div class="links">
+          <h3>
+            DC
+          </h3>
+
+          <ul>
+
+            <li>
+              <a href="">Terms Of Use</a>
+            </li>
+
+            <li>
+              <a href="">Privacy policy (New)</a>
+            </li>
+
+            <li>
+              <a href="">Ad Choices</a>
+            </li>
+
+            <li>
+              <a href="">Advertising</a>
+            </li>
+
+            <li>
+              <a href="">Jobs</a>
+            </li>
+
+            <li>
+              <a href="">Subscriptions</a>
+            </li>
+
+            <li>
+              <a href="">Talent Workshops</a>
+            </li>
+
+            <li>
+              <a href="">CPSC Certificates</a>
+            </li>
+
+            <li>
+              <a href="">Ratings</a>
+            </li>
+
+            <li>
+              <a href="">Shop Help</a>
+            </li>
+
+            <li>
+              <a href="">Contact Us</a>
+            </li>
+
+
           </ul>
         </div>
 
-        <div class="links2">
-          
-        </div>
+        <div class="links">
+          <h3>
+            SITES
+          </h3>
 
-        <div class="links3">
-          
+          <ul>
+
+            <li>
+              <a href="">DC</a>
+            </li>
+
+            <li>
+              <a href="">MAD Magazine</a>
+            </li>
+
+            <li>
+              <a href="">DC Kids</a>
+            </li>
+
+            <li>
+              <a href="">DC Universe</a>
+            </li>
+
+            <li>
+              <a href="">DC Power Visa</a>
+            </li>
+
+          </ul>
         </div>
 
       </div>
@@ -198,6 +316,49 @@ footer{
         bottom: -100px;
       }
     }
+  }
+
+  .footer-center-nav{
+    padding-left: 100px;
+    display: flex;
+
+    .links{
+      padding-top: 20px;
+      padding-left: 40px;
+
+      h3{
+        padding-top: 10px;
+        color: white;
+      }
+
+      ul{
+        
+
+        li{
+          padding-top: 15px;
+          list-style: none;
+          display: flex;
+          align-items: center;
+          
+
+
+        
+          a{
+            text-decoration: none;
+            color: white;
+            font-size: 13px;
+            
+          }
+        }
+      }
+    }
+  }
+
+
+  .footer-bottom{
+    background-color: rgb(37, 37, 37);
+    height: 150px;
+    width: 100vw;
   }
 }
 
